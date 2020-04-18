8c634d2087d246a518064735acfd59d60b998759
libaoyu <libaoyu@dhcc.com.cn>
2020-04-18 09:04:22 +0800

    测试修改之后的上传

--- src/com/mytest/demoone/HelloWorld.java
+++ src/com/mytest/demoone/HelloWorld.java
@@ -2,6 +2,8 @@ package com.mytest.demoone;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("HelloWorld");
+        System.out.println("测试一下修改上传");
     }
 }
